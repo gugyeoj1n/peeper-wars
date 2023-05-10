e297706a49620902f755c5cb803c99fa217493dc
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2023-05-10 21:14:43 +0900

    add login UI in start scene

---
+++ peeper-wars/Assets/Scripts/StartManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Misc;
+using MongoDB.Bson;
+
+public class StartManager : MonoBehaviour
+{
+    RectTransform titleTextRect;
+    public Text titleText;
+    bool isChanging = false;
+    float cnt = 0f;
+    public float nowY = 200f;
+    public float nowX = -350f;
+
+    public InputField input_id;
+    public InputField input_pw;
+    private const string MONGO_URI = "mongodb+srv://gugyeoj1n:woojin9821@peeper-wars.76mjqw0.mongodb.net/";
+    private const string DB_NAME = "Main";
+    private MongoClient mongoClient;
+    private IMongoDatabase db;
+
+    public void SceneMove()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Start()
+    {
+        titleTextRect = titleText.GetComponent<RectTransform>();
+        mongoClient = new MongoClient(MONGO_URI);
+        db = mongoClient.GetDatabase(DB_NAME);
+    }
+
+    void FixedUpdate()
+    {
+        if (cnt > 1f) isChanging = true;
+        else if (cnt < 0f) isChanging = false;
+
+        if (isChanging == false)
+        {
+            cnt += 0.01f;
+            nowY += 0.2f;
+            titleTextRect.anchoredPosition = new Vector2(nowX, nowY);
+        } else
+        {
+            cnt -= 0.01f;
+            nowY -= 0.2f;
+            titleTextRect.anchoredPosition = new Vector2(nowX, nowY);
+        }
+    }
+
+    public void TryLogin() {
+        var users = db.GetCollection<BsonDocument>("Users");
+        var filter = Builders<BsonDocument>.Filter.Eq("name", input_id.text);
+        try {
+            var checkUser = users.Find(filter).First();
+        } catch {
+            Debug.Log("NO USER ! CHECK YOUR ID.");
+            return;
+        }
+    }
+}
